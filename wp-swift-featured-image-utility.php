6fe98fa43f4cc08ef4fa64792d56259fa2f2ee69
Gary Swift <garyswiftmail@gmail.com>
2017-02-27 16:28:29 +0000

    Removed unused code

--- wp-swift-featured-image-utility.php
+++ wp-swift-featured-image-utility.php
@@ -19,31 +19,23 @@ Text Domain:       wp-swift-featured-image-utility
  * @return - $image (array)
  */
 function get_featured_image($post_id=false) {
-	// global $post;
-	// echo '1 id:'.$post_id.'<br>';
 	if(!$post_id) {
 		global $post;
 		$post_thumbnail_id = get_post_thumbnail_id( $post );
-		// echo '>>>global<br>';
 	}
 	else {
 		$post=get_post($post_id);
 		$post_thumbnail_id = $post->ID;
-		// echo '>>>from id<br>';
-		// echo "<pre>";var_dump($post);echo "</pre>";
+
 	}
-	// echo '2 id:'.$post->ID.'<br><br>';
-	$sizes = array('letterbox', 'medium_large', 'fp-small', 'fp-medium', 'fp-large', 'fp-xlarge', 'featured-small', 'featured-medium', 'featured-large', 'featured-xlarge');// 'letterbox-medium', 
+	$sizes = array('letterbox', 'medium_large', 'fp-small', 'fp-medium', 'fp-large', 'fp-xlarge', 'featured-small', 'featured-medium', 'featured-large', 'featured-xlarge');
 	
 	$image=false;
 	if ( has_post_thumbnail($post->ID) ) :
-		// echo "<pre>Getting images</pre>";
  		$image = array(); 
 		$post_thumbnail_id = get_post_thumbnail_id( $post );
 		$thumb = get_post( $post_thumbnail_id );
 	    $image['title'] = $thumb->post_title;
-	    // echo $image['title'].'<br>';
-	    // echo 'thumbid '.$thumb->ID.'<br>';
 	    $image['alt'] = get_post_meta( $thumb->ID, '_wp_attachment_image_alt', true ); //alt text
 	    $image['caption'] = $thumb->post_excerpt;
 	    $image['description'] = $thumb->post_content;
